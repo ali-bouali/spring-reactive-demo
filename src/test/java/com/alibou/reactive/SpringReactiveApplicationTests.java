f488d6758649d16fb94e5df16aa9ae3a4e2ee4e6
ali-bouali <bouali.pro@gmail.com>
2023-03-09 11:43:23 +0100

    initializing project

---
+++ src/test/java/com/alibou/reactive/SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
